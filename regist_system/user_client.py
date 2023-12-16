0af612e1d602439916203efc8882139900cda2aa
sDaigo <s8778@g.maizuru-ct.ac.jp>
2023-12-16 13:39:53 +0900

    登録システムの仕様変更→カードタッチだけで登録可能に，データベースは学籍番号と2つのカードのIdmのみ保持

--- regist_system/user_client.py
+++ regist_system/user_client.py
@@ -15,42 +15,59 @@ def index():
 
 @app.route('/add', methods=['POST'])
 def add_user():
-    username = request.form.get('username')
-    number = request.form.get('number')
-    # パスワードをハッシュ化
-    password = generate_password_hash(request.form.get('password'),method='pbkdf2:sha256')
     # /idm_inputにリダイレクト
-    return render_template('idm_input.html', username=username, number=number, password=password)
+    return render_template('idm_input.html')
 
 @app.route('/idm_input', methods=['POST'])
 def idm_input():
-    username = request.form.get('username')
-    number = request.form.get('number')
-    password = request.form.get('password')
-    flash('学生証をタッチしてください')
-    idm_univ = get_IDm()
-    # 前のフォームからユーザー名と番号を取得
-    return render_template('idm_input_bus.html', username=username, number=number,password=password,idm_univ=idm_univ)
+    student_id,idm_univ = get_univ()
+    return render_template('idm_input_bus.html', student_id = student_id, idm_univ = idm_univ)
 
 @app.route('/idm_input_bus', methods=['POST'])
 def idm_input_bus():
     # 前のフォームからユーザー名と番号を取得
-    username = request.form.get('username')
-    number = request.form.get('number')
-    password = request.form.get('password')
+    student_id = request.form.get('student_id')
     idm_univ = request.form.get('idm_univ')
-    flash('バスカードをタッチしてください')
     idm_bus = get_IDm()
-    # 収集した情報をAPIに送信
-    requests.post(f"{API_URL}/users", json={'username': username, 'password' : password, 'number': number, 'idm_univ': idm_univ, 'idm_bus': idm_bus})
-    # インデックスページにリダイレクト
-    return redirect(url_for('index'))
+
+    if idm_univ == idm_bus :
+        return redirect(url_for('index'))
+    else :
+        # 収集した情報をAPIに送信
+        requests.post(f"{API_URL}/users", json={'student_id': student_id,'idm_univ': idm_univ, 'idm_bus': idm_bus})
+        # インデックスページにリダイレクト
+        return redirect(url_for('index'))
 
 @app.route('/delete', methods=['POST'])
 def delete_user():
     user_id = request.form.get('user_id')
     requests.delete(f"{API_URL}/users/{user_id}")
     return redirect(url_for('index'))
+
+def get_univ():
+    clf = nfc.ContactlessFrontend('usb')
+    tag = clf.connect(rdwr={'on-connect': lambda tag: False})
+
+    idm, pmm = tag.polling(system_code=0xfe00)
+    tag.idm, tag.pmm, tag.sys = idm, pmm, 0xfe00
+        
+    service_code = [nfc.tag.tt3.ServiceCode(106, 0x0b)]
+    #bc_id = [nfc.tag.tt3.BlockCode(i) for i in range(4)]
+    bc_id   = [nfc.tag.tt3.BlockCode(0)]
+    bc_name = [nfc.tag.tt3.BlockCode(1)]
+
+    student_id_data = tag.read_without_encryption(service_code, bc_id)
+    name_data = tag.read_without_encryption(service_code, bc_name)
+
+    student_id = student_id_data.decode('utf-8')
+    student_id = student_id[8:14]
+    name = name_data.decode('utf-8')
+
+    idm = binascii.hexlify(tag.identifier).upper()
+    idm = idm.decode()
+ 
+    return student_id,idm
+
 
 def get_IDm():
     global idm
