1d63854fedb6bd39d4b94135b8deb38f22ca102a
sDaigo <s8778@g.maizuru-ct.ac.jp>
2023-12-21 02:14:30 +0900

    不必要なファイルの消去

--- regist_system/user_client.py
+++ regist_system/user_client.py
@@ -11,17 +11,17 @@ API_URL = "http://127.0.0.1:5000"  # Web API の URL
 def index():
     response = requests.get(f"{API_URL}/users")
     users = response.json()
-    return render_template('users.html', users=users)
+    return render_template('2_menu.html', users=users)
 
 @app.route('/add', methods=['POST'])
 def add_user():
     # /idm_inputにリダイレクト
-    return render_template('idm_input.html')
+    return render_template('3_scan.html')
 
 @app.route('/idm_input', methods=['POST'])
 def idm_input():
     student_id,idm_univ = get_univ()
-    return render_template('idm_input_bus.html', student_id = student_id, idm_univ = idm_univ)
+    return render_template('4_teiki_scan.html', student_id = student_id, idm_univ = idm_univ)
 
 @app.route('/idm_input_bus', methods=['POST'])
 def idm_input_bus():
@@ -50,7 +50,7 @@ def get_univ():
 
     idm, pmm = tag.polling(system_code=0xfe00)
     tag.idm, tag.pmm, tag.sys = idm, pmm, 0xfe00
-        
+
     service_code = [nfc.tag.tt3.ServiceCode(106, 0x0b)]
     #bc_id = [nfc.tag.tt3.BlockCode(i) for i in range(4)]
     bc_id   = [nfc.tag.tt3.BlockCode(0)]
@@ -65,7 +65,7 @@ def get_univ():
 
     idm = binascii.hexlify(tag.identifier).upper()
     idm = idm.decode()
- 
+
     return student_id,idm
 
 
